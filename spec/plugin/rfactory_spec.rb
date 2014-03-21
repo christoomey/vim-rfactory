391de0b3f8ab82bedd3a07fea76804691d36b043
Chris Toomey <chris@ctoomey.com>
2014-03-20 22:02:52 -0400

    Add trait and null spec

--- spec/plugin/rfactory_spec.rb
+++ spec/plugin/rfactory_spec.rb
@@ -3,31 +3,65 @@ require 'spec_helper'
 describe 'Rfactory' do
   %w{create build build_stubbed attributes_for}.each do |method|
     it "maps FG method '#{method}' to its factory" do
-      text = "user = #{method}(:user)"
-      spec_file = 'user_spec.rb'
-      file = write_file(spec_file, text)
+      spec_file = create_spec_file("user = #{method}(:user)")
       create_factories_file(vim.command('pwd'))
 
       vim.edit spec_file
       vim.command 'Rfactory'
 
-      factories_path = vim.command "echo expand('%')"
       line = vim.command "echo getline('.')"
 
-      expect(factories_path).to eq 'spec/support/factories.rb'
+      expect(current_path).to eq 'spec/support/factories.rb'
       expect(line).to eq 'factory :user do'
     end
   end
 
+  it 'navigates to traits if present' do
+    spec_file = create_spec_file('user = create(:user, :with_token)')
+    create_factories_file(vim.command('pwd'))
+
+    vim.edit spec_file
+    vim.command 'Rfactory'
+
+    line = vim.command "echo getline('.')"
+
+    expect(current_path).to eq 'spec/support/factories.rb'
+    expect(line).to eq 'trait :with_token do'
+  end
+
+  it 'does nothing if not on a factory call line' do
+    spec_file = create_spec_file('no factory call on this line')
+    create_factories_file(vim.command('pwd'))
+
+    vim.edit spec_file
+    vim.command 'Rfactory'
+
+    expect(current_path).to eq spec_file
+  end
+
   private
 
+  def current_path
+    vim.command "echo expand('%')"
+  end
+
+  def create_spec_file(text)
+    spec_file = 'user_spec.rb'
+    write_file(spec_file, text)
+    spec_file
+  end
+
   def create_factories_file(path)
-    FileUtils.mkdir_p "#{path}/spec/support"
-    factories_path = "#{path}/spec/support/factories.rb"
+    FileUtils.mkdir_p "spec/support"
+    factories_path = "spec/support/factories.rb"
     factories_text = normalize_string_indent <<-EOS
       FactoryGirl.define do
         factory :user do
           name 'Bob smith'
+
+          trait :with_token do
+            token 'a12bc3fe'
+          end
         end
       end
     EOS
