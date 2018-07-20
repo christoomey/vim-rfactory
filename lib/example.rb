1381aa875debf90c36b2670199029b52345e08ef
Nick Pezza <npezza93@gmail.com>
2018-07-20 11:21:18 -0400

    Add meta_data class

--- lib/example.rb
+++ lib/example.rb
@@ -1,4 +1,5 @@
 require "ripper"
+require "pry"
 
 # _dunno, root = Ripper.sexp(File.read("sample.rb"))
 
@@ -14,17 +15,42 @@ class FactoryCallFinder
   private
 
   def found?(node)
-    Node.new(node).of_type?(:method_add_arg) &&
-      node[1][0] == :fcall &&
-      node[1][1][0] == :@ident &&
-      node[1][1][1] == "create"
+    node.of_type?(:method_add_arg) &&
+      node.meta_data_of_type?(:fcall) &&
+      node.meta_data_of_child_type?(:@ident) &&
+      node.meta_data_with_content?("create")
   end
+end
+
+class MetaData
+  def initialize(meta_data)
+    @parent_type = meta_data[0]
+    @child_type = meta_data[1][0]
+    @content = meta_data[1][1]
+    @location = meta_data[1][2]
+  end
+
+  def of_type?(expected_type)
+    parent_type == expected_type
+  end
+
+  def of_child_type?(expected_type)
+    child_type == expected_type
+  end
+
+  def with_content?(expected_content)
+    content == expected_content
+  end
+
+  private
+
+  attr_accessor :content, :location, :parent_type, :child_type
 end
 
 class Node
   def initialize(node)
     @type = node[0]
-    @meta_data = node[1]
+    @meta_data = MetaData.new(node[1])
     @children = node[2]
   end
 
@@ -32,12 +58,24 @@ class Node
     type == expected_type
   end
 
+  def meta_data_of_type?(expected_type)
+    meta_data.of_type?(expected_type)
+  end
+
+  def meta_data_of_child_type?(expected_type)
+    meta_data.of_child_type?(expected_type)
+  end
+
+  def meta_data_with_content?(expected_content)
+    meta_data.with_content?(expected_content)
+  end
+
   private
 
   attr_accessor :type, :meta_data, :children
 end
 
-should_match =[:method_add_arg,
+should_match = [:method_add_arg,
    [:fcall, [:@ident, "create", [14, 13]]],
    [:arg_paren,
     [:args_add_block,
@@ -60,7 +98,7 @@ tricky_should_not_match = [:method_add_arg,
   [:should_not_match, should_not_match, false],
   [:tricky_should_not_match, tricky_should_not_match, false],
 ].each do |test_name, test_case, expected_result|
-  if FactoryCallFinder.find(test_case) == expected_result # - node
+  if FactoryCallFinder.find(Node.new(test_case)) == expected_result # - node
     puts "#{test_name} passed!"
   else
     puts "#{test_name} failed! Expected #{expected_result}."
